b63c391197bbff12d404042cebf7d5e833a47eca
shehara winod <shehara.winod123@gmail.com>
2026-01-16 18:48:18 +0530

    create omi admin command

---
+++ src/main/java/lk/cwresports/OMICardGame/Commands/OMIAdminCommands.java
@@ -0,0 +1,14 @@
+package lk.cwresports.OMICardGame.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class OMIAdminCommands implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+
+        return true;
+    }
+}
