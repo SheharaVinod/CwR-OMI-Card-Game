aff3e2c15bc5577a5c07e81088e7ef6273e377e4
shehara winod <shehara.winod123@gmail.com>
2026-01-16 18:43:57 +0530

    create main class

---
+++ src/main/java/lk/cwresports/OMICardGame/OMICardGame.java
@@ -0,0 +1,20 @@
+package lk.cwresports.OMICardGame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OMICardGame extends JavaPlugin {
+
+    private static OMICardGame instance;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        instance = this;
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
